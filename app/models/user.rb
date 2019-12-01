9626e9ba71aa37680f87f9621cf8087534f09206
Adali12 <abayiringira1994@yahoo.com>
2019-12-01 09:44:32 -0800

    user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :messages, dependent: :destroy
-  validates :firstName, presence: true
-  validates :lastName, presence: true
-  validates :email, presence: true
-  validates :bio, presence: true
-  validates :expertise, presence: true
-  validates :occupation, presence: true
-  validates :email, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  before_validation { email.downcase! }
+
   has_secure_password
 end
